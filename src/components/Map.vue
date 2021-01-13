69568702540ae03d93926246d29cae518d63d3ad
Bryan Gill <bryan@groma.io>
2021-01-13 13:59:14 +0000

    adding mapbox interface

---
+++ src/components/Map.vue
@@ -0,0 +1,67 @@
+<template>
+    <div id='map'>
+      <p>Enter access token:</p>
+      <input id='mapToken' type="text" v-model="map_arguments.accessToken"><button @click='updateMap'>load map</button>
+      <div id="mapid" v-bind:style="mapid"></div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Map',
+  data() {
+      return {
+        tile: null,
+        map: null,
+        mapid: {
+          height: '750px',
+          width: '750px'
+        },
+        map_arguments: {
+          // zoom: 18,
+          // id: 'mapbox/satellite-v9',
+          // tileSize: 512,
+          // zoomOffset: -1,
+          accessToken: '',
+          // attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          
+        }
+      }
+    },
+    mounted() {
+
+    },
+    computed() {
+
+    },
+    
+    updated() {
+    },
+    
+    methods: {
+      updateMap: function() {
+        if (this.map === null) {
+          mapboxgl.accessToken = this.map_arguments.accessToken; /* global mapboxgl */
+          this.map = new mapboxgl.Map({
+              container: 'mapid',
+              style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+              center: [-93.79956230521202, 41.71010074072029], // starting position [lng, lat]
+              zoom: 15 // starting zoom
+          });
+        } else {
+          mapboxgl.accessToken = this.map_arguments.accessToken;
+        }
+      }
+    }
+}
+</script>
+<style scoped>
+  #map {
+    height: 100%;
+    width: 100%;
+  }
+  #mapToken {
+    margin-bottom: 20px;
+    margin-right: 10px;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
+import mapboxgl from 'mapbox-gl'
+// import L from 'leaflet'
+// import '@geoman-io/leaflet-geoman-free'
+// import '@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css'
 import App from './App.vue'
+
+window.mapboxgl = require('mapbox-gl');
 
 createApp(App).mount('#app')
