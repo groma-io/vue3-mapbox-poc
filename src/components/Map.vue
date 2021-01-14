d3b3636dd7fe0d1e5ecdf5a8795bc40824e08ba8
Bryan Gill <bryan@groma.io>
2021-01-14 18:59:01 +0000

    added landview

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,12 +1,13 @@
 <template>
     <div id='map'>
-      <p>Enter access token:</p>
-      <input id='mapToken' type="text" v-model="map_arguments.accessToken"><button @click='updateMap'>load map</button>
       <div id="mapid" v-bind:style="mapid"></div>
     </div>
 </template>
 
 <script>
+
+import axios from 'axios';
+
 export default {
   name: 'Map',
   data() {
@@ -14,54 +15,103 @@ export default {
         tile: null,
         map: null,
         mapid: {
-          height: '750px',
-          width: '750px'
+          height: '100vh',
+          width: '100vw'
         },
-        map_arguments: {
-          // zoom: 18,
+        landview_arguments: {
+          base_url: 'https://tiles.makeloveland.com',
+          url: '/api/v1/parcels?format=mvt&token=',
+          accessToken: process.env.VUE_APP_LANDVIEW_TOKEN,
+          
+          
+        },
+        landview_layer_data: [],
+        mapbox_arguments: {
           // id: 'mapbox/satellite-v9',
           // tileSize: 512,
           // zoomOffset: -1,
-          accessToken: '',
+          zoom: 15,
+          html_container: 'mapid',
+          style: 'mapbox://styles/mapbox/outdoors-v11',
+          // accessToken: ,
+          center: [-93.79956230521202, 41.71010074072029]
           // attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
           
         }
       }
     },
     mounted() {
-
+      console.log(process.env.VUE_APP_MAPBOX_TOKEN);
+      // mapboxgl.accessToken =  process.env.VUE_APP_MAPBOX_TOKEN /* global mapboxgl */
+      // this.map = new mapboxgl.Map({
+      //     container: this.mapbox_arguments.html_container,
+      //     style: this.mapbox_arguments.style, // stylesheet location
+      //     center: this.mapbox_arguments.center, // starting position [lng, lat]
+      //     zoom: this.mapbox_arguments.zoom // starting zoom
+      // });
+      // this.map.showTileBoundaries = true;
     },
     computed() {
 
     },
     
-    updated() {
+    created() {
+      // let url = this.landview_arguments.base_url + this.landview_arguments.url + this.landview_arguments.accessToken;
+      // axios.get(url)
+      // .then(response => {
+      //   this.landview_layer_data = response.data;
+      //   this.map.addSource(this.landview_layer_data.id, {
+      //       type: 'vector',
+      //       tiles: this.landview_layer_data.tiles
+      //     });
+          
+      //   this.map.addLayer({
+      //     'id': 'parcels',
+      //     'type': 'line',
+      //     'source': this.landview_layer_data.id,
+      //     'source-layer': this.landview_layer_data.id,
+      //     'minzoom': 13,
+      //     'maxzoom': 20,
+      //     layout: {
+      //       visibility: 'visible'
+      //     },
+      //     'paint': {
+      //       'line-color': '#649d8d'
+      //     }
+      //   });          
+      // })
+      // .catch(e => {
+      //   console.log(e);
+      // })
     },
-    
+
+      // this.map.addSource('mapbox-dem', {
+      //   'type': 'raster-dem',
+      //   'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
+      //   'tileSize': 512,
+      //     'maxzoom': 14,
+      //     'center': [0, 0]
+      // });
+      
+      //           // add the DEM source as a terrain layer with exaggerated height
+      // this.map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 1.5 });
+           
+      //     // add a sky layer that will show when the map is highly pitched
+      // this.map.addLayer({
+      //   'id': 'sky',
+      //   'type': 'sky',
+      //   'paint': {
+      //   'sky-type': 'atmosphere',
+      //   'sky-atmosphere-sun': [0.0, 0.0],
+      //   'sky-atmosphere-sun-intensity': 15
+      //   }
+      // })
+
     methods: {
       updateMap: function() {
-        if (this.map === null) {
-          mapboxgl.accessToken = this.map_arguments.accessToken; /* global mapboxgl */
-          this.map = new mapboxgl.Map({
-              container: 'mapid',
-              style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-              center: [-93.79956230521202, 41.71010074072029], // starting position [lng, lat]
-              zoom: 15 // starting zoom
-          });
-        } else {
-          mapboxgl.accessToken = this.map_arguments.accessToken;
-        }
       }
     }
 }
 </script>
 <style scoped>
-  #map {
-    height: 100%;
-    width: 100%;
-  }
-  #mapToken {
-    margin-bottom: 20px;
-    margin-right: 10px;
-  }
 </style>
